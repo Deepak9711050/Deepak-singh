07ff67fee4546c4b0617e0b8f77cb20ce9328237
Deepak9711050 <117795236+Deepak9711050@users.noreply.github.com>
2023-03-10 10:53:05 +0530

    Add files via upload

---
+++ ArrayExample.java
@@ -0,0 +1,12 @@
+public class ArrayExample {
+    public static void main(String[] args) {
+        int ages[] = new int[4];
+        ages[0] = 35;
+        ages[1] = 46;
+        ages[2]= 55;
+       
+        System.out.println(ages[0]);
+        System.out.println(ages[1]);
+        System.out.println(ages[2]);
+    }
+}
